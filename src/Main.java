e556b0bba72f8d7b7606ffff8ab0c0a46e3c670f
Chris <Chrisnippert22@gmail.com>
2019-12-10 16:25:05 -0500

    Adding encryption that works
    Still working on login though

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,236 @@
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.Signature;
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
+import java.io.*;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Iterator;
 import java.util.Scanner;
+
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static Account acc = new Account();
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
+
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("1: Encrypt\n2: Decrypt\n>>> ");
+        System.out.print("1: Create Account\n2: Login\n3: Delete All\n>>> ");
         String input  = scanner.nextLine();
         if(input.equals("1")){
-            try {
-                System.out.println("Input the text you would like to encrypt.\n>>> ");
-                input = scanner.nextLine();
-                encrypt(input);
-            }
-            catch (Exception e){
+                System.out.print("Username: ");
+                String user = scanner.next();
 
+                System.out.print("Password: ");
+                String textPassword = scanner.next();
+
+                System.out.print("Password (again): ");
+                String passwordCheck = scanner.next();
+                if(textPassword.equals(passwordCheck)){
+                    createUser(user, textPassword);
+                }
+                else{
+
+                }
             }
-        }
         else if(input.equals("2")){
-            try {
-                //decrypt(input);
-            }
-            catch (Exception e){
+            System.out.print("Username: ");
+            String user = scanner.next();
 
-            }
+            System.out.print("Password: ");
+            String textPassword = scanner.next();
+
+            login(user, textPassword);
+
+        }
+        else if(input.equals(3)){
+
         }
 
     }
-    public static void encrypt(String usrinput) throws Exception{
-        //Creating a Signature object
-        Signature sign = Signature.getInstance("SHA256withRSA");
 
-        //Creating KeyPair generator object
-        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+    private static void createKey(){
+        JSONObject keySave = new JSONObject();
 
-        //Initializing the key pair generator
-        keyPairGen.initialize(2048);
+        keySave.put("key",key);
 
-        //Generating the pair of keys
-        KeyPair pair = keyPairGen.generateKeyPair();
+        StringWriter out = new StringWriter();
+        try {
+            keySave.writeJSONString(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        //Creating a Cipher object
-        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-
-        //Initializing a Cipher object
-        cipher.init(Cipher.ENCRYPT_MODE, pair.getPublic());
-
-        //Adding data to the cipher
-        byte[] input = usrinput.getBytes();
-        cipher.update(input);
-
-        //encrypting the data
-        byte[] cipherText = cipher.doFinal();
-        System.out.println(new String(cipherText, "UTF8"));
-
-        cipher.init(Cipher.DECRYPT_MODE, pair.getPrivate());
-        byte[] plainText = cipher.doFinal();
-        System.out.println(plainText);
+        String jsonText = out.toString();
+        try (FileWriter file = new FileWriter("key.json")) {
+            file.write(jsonText);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-//    public static String decrypt(String usrinput, ){
-//
-//    }
+    public static void login(String username, String password){
+        JSONParser parser = new JSONParser();
+        String user;
+        String pass;
+
+        try (Reader reader = new FileReader("users.json")) {
+
+            JSONObject jsonObject = (JSONObject) parser.parse(reader);
+            System.out.println(jsonObject);
+            BufferedReader br = new BufferedReader(new FileReader("users.json"));
+            try {
+                setKey(password);
+                Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+                cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+                String userInput = Base64.getEncoder().encodeToString(cipher.doFinal(username.getBytes()));
+                String passInput = Base64.getEncoder().encodeToString(cipher.doFinal(password.getBytes()));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    user = (String)jsonObject.get("username");
+                    pass = (String)jsonObject.get("password");
+                    if(userInput.equals(user) && passInput.equals(pass)){
+
+                    }
+                }
+            } finally {
+                br.close();
+            }
+
+            // loop array
+            JSONArray msg = (JSONArray) jsonObject.get("messages");
+            Iterator<String> iterator = msg.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (Exception e){
+            System.out.print(e);
+        }
+    }
+    // Creates user
+    public static void createUser(String username, String password){
+        try
+        {
+            setKey(password);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            acc.username = Base64.getEncoder().encodeToString(cipher.doFinal(username.getBytes()));
+            acc.password = Base64.getEncoder().encodeToString(cipher.doFinal(password.getBytes()));
+            JSONObject user = new JSONObject();
+
+            user.put("username",acc.username);
+            user.put("password",acc.password);
+
+            StringWriter out = new StringWriter();
+            try {
+                user.writeJSONString(out);
+                System.out.println("You have successfully created a new user!");
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            String jsonText = out.toString();
+            try (FileWriter file = new FileWriter("users.json")) {
+                file.write(jsonText);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error while encrypting: " + e.toString());
+        }
+    }
+    // Creates a credential entry
+    public static void createCredential(){
+
+    }
+    public static void setKey(String myKey)
+    {
+        MessageDigest sha = null;
+        try {
+            key = myKey.getBytes("UTF-8");
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, "AES");
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String encrypt(String strToEncrypt)
+    {
+        try
+        {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error while encrypting: " + e.toString());
+        }
+        return null;
+    }
+
+    public static String decrypt(String strToDecrypt, String secret)
+    {
+        try
+        {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error while decrypting: " + e.toString());
+        }
+        return null;
+    }
 }
+
+
+//    SecureRandom random = new SecureRandom();
+//    byte[] salt = new byte[16];
+//            random.nextBytes(salt);
+//
+//                    KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 256); // AES-256
+//                    SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+//                    byte[] key = f.generateSecret(spec).getEncoded();
+//                    SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
+//
+//                    byte[] ivBytes = new byte[16];
+//                    random.nextBytes(ivBytes);
+//                    IvParameterSpec iv = new IvParameterSpec(ivBytes);
+//
+//                    Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+//                    c.init(Cipher.ENCRYPT_MODE, keySpec, iv);
+//                    byte[] encValue = c.doFinal(password.getBytes());
+//
+//                    byte[] finalCiphertext = new byte[encValue.length+2*16];
+//                    System.arraycopy(ivBytes, 0, finalCiphertext, 0, 16);
+//                    System.arraycopy(salt, 0, finalCiphertext, 16, 16);
+//                    System.arraycopy(encValue, 0, finalCiphertext, 32, encValue.length);
+//
+//                    return finalCiphertext.toString();
